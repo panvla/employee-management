603bd3ada4b1daf731bbcb84dbb02be4b5d99ea4
panvla <pandvlado@gmail.com>
2022-09-23 00:48:41 +0200

    added initial project.closes #1.

---
+++ src/main/java/com/vladimirpandurov/employeemanager/model/Employee.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.employeemanager.model;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    private String employeeCode;
+}
